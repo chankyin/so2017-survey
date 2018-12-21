8907b485acc56b0168d64a31e2e56c4aca742224
chankyin <chankyin1970@gmail.com>
2018-12-21 22:24:29 +0800

    Fixed install script

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-wget -O data.zip "https://drive.google.com/uc?export=download&id=1_9On2-nsBQIw3JiY43sWbrF8EjrqrR4U"
-unzip -d data.zip
-
+curl -Lo data.zip "https://drive.google.com/uc?export=download&id=0B6ZlG_Eygdj-c1kzcmUxN05VUXM"
+unzip data.zip
+npm install
+node clean.js
